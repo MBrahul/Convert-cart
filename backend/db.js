3dbfda88e44bdc523869038ebd2772111c943f56
vishvjeetprajapati <vishvjeetprajapati138@gmail.com>
2025-10-19 21:28:32 +0530

    mysql deployment 2.0

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connection = await mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    port:process.env.DB_PORTm|| 3306,
+    port:process.env.DB_PORT|| 3306,
     password: process.env.DB_PASSWORD,
 });
 await connection.query('CREATE DATABASE IF NOT EXISTS woocommerce_products');
@@ -17,7 +17,7 @@ console.log('Database ensured.');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port:process.env.DB_PORTm|| 3306,
+    port:process.env.DB_PORT|| 3306,
     dialect: 'mysql',
     logging: false,
 });
